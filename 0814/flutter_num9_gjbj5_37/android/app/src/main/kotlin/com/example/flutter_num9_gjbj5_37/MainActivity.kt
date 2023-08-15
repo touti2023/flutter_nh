4f2a43c3d0fa83828ee10609bc1c35d2db4d9fca
touti2023 <123626581+touti2023@users.noreply.github.com>
2023-08-15 11:38:08 +0900

    no message

---
+++ 0814/flutter_num9_gjbj5_37/android/app/src/main/kotlin/com/example/flutter_num9_gjbj5_37/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_num9_gjbj5_37
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 0814/flutter_num9_kptkf/android/app/src/main/java/com/example/flutter_num9_kptkf/MainActivity.java
@@ -0,0 +1,6 @@
+package com.example.flutter_num9_kptkf;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
